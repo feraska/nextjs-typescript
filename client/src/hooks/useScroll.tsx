b2af0584a21e513b2b9419b46bc63781478be691
feraska <feras.94@hotmail.com>
2024-09-28 10:03:32 +0300

    fix modal

--- client/src/hooks/useScroll.tsx
+++ client/src/hooks/useScroll.tsx
@@ -1,20 +1,35 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 
 const useScroll = (id:string) => {
-    const scrollPosition = useRef({ x: 0, y: 0 })
+    // const scrollPosition = useRef({ x: 0, y: 0 })
+    const [scrollPosition,setScrollPosition] = useState({ x: 0, y: 0 })
     const scroll = () => {
-        scrollPosition.current = {
-            x: window.scrollX,
-            y: window.scrollY,
-          };
-    }
-    useEffect(()=> {
-    
+        // scrollPosition.current = {
+        //     x: window.scrollX,
+        //     y: window.scrollY,
+        //   };
+        if(scrollX === 0 && scrollY === 0) {
+            return
+        }
         if(id) {
             return
         }
+        setScrollPosition({ x: window.scrollX, y: window.scrollY,})
+      
+        // window.scrollTo(window.scrollX,window.scrollY)
+        // console.log(window.scrollX,window.scrollY)
+    }
+    useEffect(()=> {
+    
+        // if(id) {
+        //     return
+        // }
+        //window.scrollTo(scrollPosition.current.x,scrollPosition.current.y)
+        //window.scrollTo(scrollPosition.x,scrollPosition.y)
+        
+        window.scrollTo(scrollPosition.x,scrollPosition.y)
         window.addEventListener("scroll",scroll)
-        window.scrollTo(scrollPosition.current.x,scrollPosition.current.y)
+        
         
         
         
@@ -22,5 +37,12 @@ const useScroll = (id:string) => {
             window.removeEventListener("scroll",scroll)
         }
        },[id])
+
+
+    // useEffect(()=> {
+       
+       
+    // },[id])   
+
 }
 export default useScroll
