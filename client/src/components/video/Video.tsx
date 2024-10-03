9e02789863ab2e7a6ec5864c5dd38a49bbc42c50
feraska <feras.94@hotmail.com>
2024-10-03 13:55:17 +0300

    video from youtube

--- client/src/components/video/Video.tsx
+++ client/src/components/video/Video.tsx
@@ -5,11 +5,13 @@ import "./video.scss"
 import { IoVolumeHighOutline, IoVolumeMuteOutline } from "react-icons/io5"
 import { useRouter } from "next/navigation"
 import { card } from "@/interfaces/card"
+import useVideo from "@/hooks/useVideo"
 
 const Video:React.FC<{item:card,isList?:boolean}> = ({item,isList=false}) => {
     const router = useRouter()
     const newRef = useRef<HTMLDivElement>(null);
     const [isMuted,setIsMuted] = useState(true)
+    const {data,loading,error} = useVideo(`https://api.themoviedb.org/3/movie/${item.id}/videos`)
     const getMovie = (e:MouseEvent<HTMLDivElement>) => {
         if(handleOutsideClick(e)) {
         router.push(`/watch/${item?.id}`)
@@ -28,13 +30,17 @@ const Video:React.FC<{item:card,isList?:boolean}> = ({item,isList=false}) => {
     return(
         <div className="video">
             <div className="show" onClick={getMovie}>
-                        {/* <iframe autoCapitalize=""
-            src="https://www.youtube.com/embed/TYljxL4WeRo">
-            </iframe> */}
-            <video src="https://res.cloudinary.com/dpel2vfvq/video/upload/v1715930615/video_bie12o.mp4" autoPlay loop muted={isMuted} />
-            <div className="volume" ref={newRef}>
+                        <iframe autoCapitalize=""
+            src={`https://www.youtube.com/embed/${data?.results[0].key}?autoplay=1&mute=1`}
+            
+            frameBorder="0" 
+            >
+            </iframe>
+            {/* <video src="https://www.youtube.com/watch?v=sjo6ajQ6uTs" autoPlay loop muted={isMuted} /> */}
+
+            {/* <div className="volume" ref={newRef}>
             {isMuted?<IoVolumeMuteOutline/>:<IoVolumeHighOutline />}
-            </div>
+            </div> */}
             </div>
             <Details item={item}  isList={isList}/>
         </div>
--- client/src/page/watch/Watch.tsx
+++ client/src/page/watch/Watch.tsx
@@ -2,16 +2,20 @@
 import { IoArrowBack } from "react-icons/io5"
 
 import "./watch.scss"
-import { useRouter } from "next/navigation"
+import { useParams, useRouter } from "next/navigation"
 import useGlobal from "@/hooks/useGloabal"
 import { useContext } from "react"
 import { AuthContext } from "@/context/AuthContext"
 import Loading from "@/components/loading/Loading"
 import { useAppSelector } from "@/redux/hooks"
+import useVideo from "@/hooks/useVideo"
 const Watch = () => {
     const {state} = useContext(AuthContext)
     const router = useRouter()
     const login = useAppSelector((state)=>state.user.login)
+    const params = useParams()
+    const id = params.id
+    const {data,loading,error} = useVideo(`https://api.themoviedb.org/3/movie/${id}/videos`)
     useGlobal()
     
     if(login === 2) {
@@ -26,7 +30,12 @@ const Watch = () => {
           <div className="back">
             <IoArrowBack  onClick={() => router.back()} className="back-icon"/>
           </div>
-          <video src={"https://res.cloudinary.com/dpel2vfvq/video/upload/v1715930615/video_bie12o.mp4"} autoPlay loop controls  />
+          <iframe autoCapitalize=""
+            src={`https://www.youtube.com/embed/${data?.results[0].key}?autoplay=1&mute=1`}
+            
+            frameBorder="0" 
+            >
+            </iframe>
         </div>
     )
 }
