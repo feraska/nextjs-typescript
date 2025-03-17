c2503df873dc2c97bba7ace423cdcee9ce4ae1c1
feraska <feras.94@hotmail.com>
2025-03-17 21:39:05 +0200

    fix ser model notification

--- server/models/notification.ts
+++ server/models/notification.ts
@@ -1,15 +1,10 @@
 import mongoose from "mongoose";
 
 const notificationSchema = new mongoose.Schema({
-    users: Array,
     msg: {
         type: String, required: true
     },
-    sender: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "users",
-        required: true,
-      },
+
 },
 {
     timestamps:true
