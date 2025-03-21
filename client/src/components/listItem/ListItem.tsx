1ed0d6029cfa6d0b1474b70f3033c30a7e508520
feraska <feras.94@hotmail.com>
2025-03-21 12:39:37 +0200

    fix list item

--- client/src/components/listItem/ListItem.tsx
+++ client/src/components/listItem/ListItem.tsx
@@ -1,23 +1,18 @@
 "use client"
-import React, { useState } from "react"
+import React from "react"
 import "./listItem.scss"
 import useInfo from "../../hooks/useInfo"
-import dynamic from "next/dynamic"
-const Video = dynamic(()=>import("@/components/video/Video"),{ssr:false})
-import Image from "next/image"
+import CardItem from "../cardItem/CardItem"
 
 const ListItem:React.FC<{id:number}> = ({id}) => {
     const {data:item} = useInfo(`https://api.themoviedb.org/3/movie/${id}`)//information about a movie
-    const [hover,setHover] = useState(false)//mouse hover
+    
     if(!item) {
         return
     }
 
     return(
-        <li className="list-item" onMouseOver={()=>setHover(true)} onMouseLeave={()=>setHover(false)}>
-             <Image width={100} height={100} alt="" src={`https://image.tmdb.org/t/p/w500/${item?.poster_path?item?.poster_path:item?.backdrop_path}` }/>
-             {hover&&<Video item={item} />}
-        </li>
+       <CardItem key={id} item={item}/>
     )
 }
 export default ListItem
