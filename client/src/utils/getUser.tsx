324c9722cc11e2e4834937c1c84a6b6406e14434
feraska <feras.94@hotmail.com>
2025-04-05 15:26:26 +0300

    add env

--- client/src/utils/getUser.tsx
+++ client/src/utils/getUser.tsx
@@ -4,7 +4,7 @@ import { io, Socket } from "socket.io-client";
 // const nav = navigator.userAgent
 // export const isMobile = nav.match(/Mobile/i)?true:false;
 export const  mobileWidth = window.matchMedia("(max-width: 768px)").matches; 
-export const socket: Socket = io("https://nextjs-typescript-1.onrender.com")
+export const socket: Socket = io(process.env.NEXT_PUBLIC_API_SOCKET??"")
 
 
 
